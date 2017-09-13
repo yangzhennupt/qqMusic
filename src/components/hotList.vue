166ca4ec5e6ca2f9cd443f2eb77c9179c61bdb08
杨振 <yangzhennupt@163.com>
2017-09-13 09:25:01 +0800

    播放样式修改

--- src/components/hotList.vue
+++ src/components/hotList.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="hotList">
         <div class="img-filter">
-            <img :src="logoImg">
+            <img :src="cdInfo.logo">          
         </div>
+        <ul class="hotList-detail">
+            <li v-for="(item,index) in hotList" :key="item.songid" @click="playSong(item)" class="clearfix">
+               <span class="list-no">{{++index}}</span>{{item.data.name}}
+            </li>
+        </ul>
     </div>
 </template>
 <script>
@@ -12,7 +17,7 @@ export default {
         return {
             hotId: this.$route.params.id,
             hotList:[],
-            logoImg:''
+            cdInfo:{}
         }
     },
     computed: {
@@ -23,7 +28,8 @@ export default {
     	  this.$store.dispatch('getHotDetail', {
             id: this.hotId
         }).then(res=>{
-            this.logoImg=res.data.cdlist[0].logo;
+            console.log(res);
+            this.cdInfo=res.data.cdlist[0];
             let temList=res.data.cdlist[0].songlist;
             temList.forEach((item,index)=>{
                  if(index===10)return;
@@ -31,24 +37,43 @@ export default {
                     data:{
                         songid:null,
                         albummid:null,
-                        singer:[]
+                        singer:[],
+                        name:null
                     }
                 };
                 temObj.data.songid=item.id;
                 temObj.data.albummid=item.album.mid;
                 temObj.data.singer=item.singer;
+                temObj.data.name=item.name;
                 this.hotList.push(temObj);
                
             })
-        	console.log(this.hotList);
+        	//console.log(this.hotList);
         })
     },
     methods: {
+        playSong(item){
+             this.$store.commit('changAudio', {
+                "songid": item.data.songid,
+                "albummid": item.data.albummid
+            });
+            //获取当前的index;
+            let curIndex=this.$store.state.audio.index;
+            //在当前index位置插入一个数组元素;
+            this.$store.state.musicList.splice(++curIndex,0,item);
 
+            this.$store.commit('changeMusic',curIndex);
+             
+            //改变播放状态
+            this.$store.commit('play', true);
+            //播放
+            this.$store.state.dom.play();
+        }
     }
 }
 </script>
 <style scoped lang="scss">
+
     .hotList{
         position: absolute;
         top: 0;
@@ -56,5 +81,42 @@ export default {
         left: 0;
         right: 0;
         background: #fff;
+    }
+    .img-filter{
+        img{
+            width:100%;
+            height:300px;
+            float: left;
+        }
+        p{
+            margin: 0;
+        }
+    }
+    .hotList-detail{
+        list-style: none;
+        position: absolute;
+        top: 300px;
+        bottom: 0;
+        left:0;
+        right: 0;
+        overflow-y: auto;
+        overflow-x:hidden;
+        padding: 0;
+        margin: 0;
+            li{
+                width: 100%;
+                height: 45px;
+                line-height: 45px;
+                padding-left: 40px;
+                text-align: left;
+                &:nth-child(even){
+                    background: #fcfcfc;
+                }
+                .list-no{
+                    float: left;
+                    margin-left: -20px;
+
+                }
+            }
     }
 </style>
