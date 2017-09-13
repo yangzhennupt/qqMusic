6dd792fbd0f1f2fbb8851403c6ee985c51e6d6a4
杨振 <yangzhennupt@163.com>
2017-09-13 09:17:47 +0800

    插件更新后出现样式错误，修复此bug

--- src/main.js
+++ src/main.js
@@ -3,15 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+require('swiper/dist/css/swiper.css');
 // import axios from 'axios';
 // import VueAxios from 'vue-axios';
-import 'normalize.css';
+//import 'normalize.css';
 import Vuex from 'vuex';
 import vueResource from 'vue-resource';
 //import VueAwesomeSwiper from 'vue-awesome-swiper';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-default/index.css';
-Vue.use(ElementUI);
+//import ElementUI from 'element-ui';
+//import 'element-ui/lib/theme-default/index.css';
+//Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 //Vue.use(VueAxios,axios);
