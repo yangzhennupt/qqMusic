2ecded603c78ac2ccf585f4eea35c394cc66a452
杨振 <yangzhennupt@163.com>
2018-01-15 17:01:04 +0800

    修改端口号，有个接口有referer问题，待尝试referer伪造

--- src/config/store/store_action.js
+++ src/config/store/store_action.js
@@ -159,22 +159,21 @@ export  const ACTION = {
             return new Promise((resolve,reject)=>{
                 Vue.http.jsonp('https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg',{
                     params:{
-                        g_tk:5381,
-                        uin:0,
-                        format:'jsonp',
-                        inCharset:'utf-8',
-                        outCharset:'utf-8',
-                        notice:0,
-                        platform:'h5',
-                        needNewCode:1,
-                        new_format:1,
-                        pic:500,
-                        disstid:id,
                         type:1,
                         json:1,
                         utf8:1,
                         onlysong:0,
-                        nosign:1,
+                        disstid:id,
+                        format:'jsonp',
+                        g_tk:5381,
+                        loginUin:0,
+                        hostUin:0,
+                        format:'jsonp',
+                        inCharset:'utf8',
+                        outCharset:'utf-8',
+                        notice:0,
+                        platform:'yqq',
+                        needNewCode:0,
                         _: new Date().getTime()
                        
                     },
